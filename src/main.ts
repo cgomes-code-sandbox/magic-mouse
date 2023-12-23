c021f44fa033ff9eeb0df5ac674ba0711c968301
Carlos Gomes <carlos.algms@gmail.com>
2023-12-23 18:30:59 +0100

    Added magic falling stars

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,55 @@
-const maxStack = 10;
+(() => {
+  const appDiv: HTMLDivElement = document.querySelector('#App')!;
+  const starSvgTemplate = document.querySelector<HTMLTemplateElement>('template')!;
+  let lastAdded = { x: 0, y: 0 };
+  const distance = 25;
+  const starColors = ['249 146 253', '252 254 255'];
+  const starSizes = ['1.4rem', '1rem', '0.6rem'];
+  const starAnimations = ['star-fall-1', 'star-fall-2', 'star-fall-3'];
+
+  function getRandomValue(ar: string[]) {
+    const randomIndex = Math.floor(Math.random() * ar.length);
+    return ar[randomIndex];
+  }
+
+  function calculateDistance(x1: number, y1: number, x2: number, y2: number): number {
+    const xDiff = x2 - x1;
+    const yDiff = y2 - y1;
+    const distance = Math.sqrt(xDiff ** 2 + yDiff ** 2);
+    return distance;
+  }
+
+  function createStar(x: number, y: number) {
+    const fragment = starSvgTemplate.content.cloneNode(true) as DocumentFragment;
+    const star = fragment.children[0] as SVGElement;
+    star.classList.add('star');
+    star.style.left = `${x}px`;
+    star.style.top = `${y}px`;
+    star.style.animationName = getRandomValue(starAnimations);
+    star.style.animationDuration = 'var(--star-animation-ms)';
+    star.style.color = `rgb(${getRandomValue(starColors)})`;
+    star.style.width = star.style.height = getRandomValue(starSizes);
+    return star;
+  }
+
+  function handleMove(e: MouseEvent) {
+    const { offsetX: x, offsetY: y } = e;
+
+    if (calculateDistance(x, y, lastAdded.x, lastAdded.y) < distance) {
+      return;
+    }
+
+    lastAdded = { x, y };
+
+    const star = createStar(x, y);
+    appDiv.appendChild(star);
+
+    // setTimeout(() => {
+    //   star.remove();
+    // }, 1500);
+  }
+
+  appDiv.addEventListener('mousemove', (e) => {
+    requestAnimationFrame(() => handleMove(e));
+  });
+})();
